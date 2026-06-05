97e53d2dd5171f522eb64bf3a7afb52672783ffb
Riskr999 <fbax941@gmail.com>
2026-06-05 11:50:26 +0530

    Update and rename Problem_1 to Problem_1.java

---
+++ Week_1/Day_4/Problem_1.java
@@ -0,0 +1,20 @@
+// matrix Diagonal sum
+class Soluion{
+public int diagonalSum(int[][] mat){
+  int sum = 0;
+  int n = mat.length;
+  for(int i =0;i<n;i++){
+    for(int  j =0;j<n;j++){
+          if(i == j ||  i+j = n-1){
+            sum += mat[i][j];
+          }
+        }
+    
+    }
+    return sum;
+
+  }
+
+
+}
+
